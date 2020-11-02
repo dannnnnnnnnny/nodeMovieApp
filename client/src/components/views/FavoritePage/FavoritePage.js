16b7bad25847cc348ee6ca228850c1ad8d336f1c
dh_kim <ehdgnl5249@naver.com>
2020-11-02 16:39:34 +0900

    favorite 페이지 생성

---
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./favorite.css"
+function FavoritePage() {
+    return (
+        <div style={{width: '85%', margin: '3rem auto'}}>
+            <h2> Favorite Movies </h2>
+            <hr />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Movie Title</th>
+                        <th>Movie Runtime</th>
+                        <td>Remove from favorites</td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+            FavoritePage
+        </div>
+    )
+}
+
+export default FavoritePage
